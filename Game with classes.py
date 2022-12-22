0bb0dedcf30d2d6b1443717bdcf5344ceb87af9b
peshchtekej <114681226+ssnezhok1@users.noreply.github.com>
2022-12-22 18:31:50 +0300

    1.3.0

--- Game with classes.py
+++ Game with classes.py
@@ -1,8 +1,16 @@
-import random, time, text
+import text
+
+import random
+import time
+
+import pygame
+
+pygame.init()
 class Hero:
     '''Этот класс описывает героя.'''
     
     
+    SKIN=pygame.image.load('pics\просто красивый замок.jpg').convert()
     BASE_HP=1000
     DMG=100
     BASE_WP='GUN'
@@ -30,6 +38,7 @@ class Mage(Hero):
     '''Это класс Мага.'''
 
 
+    SKIN=pygame.image.load('pics\маг.jpg').convert()
     DMG=400
     BASE_WP='Молния'
     BASE_HP=500
@@ -41,6 +50,7 @@ class Warrior(Hero):
     '''Это класс Воина.'''
     
     
+    SKIN=pygame.image.load('pics\воин с мечом в замке смотрит на солнце.jpg').convert()
     DMG=100
     BASE_WP='Меч'
     BASE_HP=1500
@@ -51,11 +61,21 @@ class Hiler(Hero):
     '''Это класс Целителя.'''
     
     
+    SKIN=pygame.image.load('pics\аниме с палкой на фоне реки и леса.jpg').convert()
     DMG=50
     BASE_WP='Палкой в животик'
     BASE_HP=1000
     ULTA='Палкой в глаз'
     ULTA_DMG=100
+
+
+class Badmalaj(Hero):
+    '''Это класс врага.'''
+    
+    SKIN=pygame.image.load('pics\бэдбой.jpg').convert()
+    DMG=100
+    BASE_WP='Камень в лицо'
+    BASE_HP=250
 
 
 def choose_chel() -> Hero:
